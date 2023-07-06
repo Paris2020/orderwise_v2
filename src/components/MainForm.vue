992b8c6bd3f1f63df2b8315205a33aea39528dc4
Paris2020 <nelly.moseki@amazeelabs.com>
2023-07-06 19:49:27 +0200

    Feature - Fix webpack issues.

---
+++ src/components/MainForm.vue
@@ -0,0 +1,44 @@
+<script setup>
+    import InputField from "./global/InputField.vue";
+    import { reactive } from "vue"
+    
+</script>
+
+<template>
+    <div class="custom-form">
+        <header class="header">
+            <img src="/images/logo.png" alt="logo" />
+        </header>
+        <div class="wrapper">
+
+            <h2>Please give your feedback below</h2>
+
+            <div class="text-blocks">
+                <dl class="text-block">
+                    <dt>01. Click 'New line' if you need more space.</dt>
+                    <dd></dd>
+                </dl>
+                <dl class="text-block">
+                    <dt>02. Click 'X' to remove the current line.</dt>
+                    <dd></dd>
+                </dl>
+                <dl class="text-block">
+                    <dt>03. Your review must be at least 50 characters but no more than 1000 characters.</dt>
+                    <dd></dd>
+                </dl>
+            </div>
+
+            <form method="post" id="feedback">
+                <div class="row">
+                    <div class="input-fields">
+                        <!--COMPONENT.input-field -->
+                        <input-field></input-field>
+
+                        <div id="new-row" class="new-row"></div>
+                        <button id="add-row" type="button" class="btn unclear large">Add Row</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/*const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, './src/scss/_style.scss'),
+    output: {
+        path: path.resolve(__dirname, './public'),
+        publicPath: "/css",
+        filename:  'js/bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.(s(a|c)ss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'style-loader',
+                    'css-loader', 
+                    'sass-loader'
+                ]
+            },
+
+        ]
+    },
+    plugins: [
+        // Where the compiled SASS is saved to
+        new MiniCssExtractPlugin({
+            filename: './public/css/style.css',
+        })
+    ],
+};*/
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
